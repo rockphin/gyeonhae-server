0cb35d34e9daecec642429000d7e9d305680d26f
CraftyDragon678 <craftydragon678@gmail.com>
2021-09-07 07:03:30 +0900

    feat: barcode recognize

--- src/api/routers/recognize.ts
+++ src/api/routers/recognize.ts
@@ -1,16 +1,27 @@
 import HttpException from '@src/exceptions/HttpException';
+import BarcodeService, { BarcodeResult } from '@src/services/Barcode';
+import { celebrate, Joi } from 'celebrate';
 import { Router } from 'express';
 import expressAsyncHandler from 'express-async-handler';
+import Container from 'typedi';
 
 const route = Router();
 
 const recognize = (app: Router) => {
   app.use('/recognize', route);
 
-  route.get(
-    '/barcode',
-    expressAsyncHandler((req, res) => {
-      throw new HttpException(501);
+  route.get<{ code: number }, BarcodeResult[], never>(
+    '/barcode/:code',
+    celebrate({
+      params: {
+        code: Joi.number(),
+      },
+    }),
+    expressAsyncHandler(async (req, res) => {
+      const { code } = req.params;
+      const barcodeService = Container.get(BarcodeService);
+      const result = await barcodeService.getBarcodeInfo(code);
+      res.json(result);
     }),
   );
 };
